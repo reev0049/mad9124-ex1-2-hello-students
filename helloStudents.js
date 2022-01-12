518bcc8f403c02baa273a4996a1055e1854b9804
reev0049 <90356691+reev0049@users.noreply.github.com>
2022-01-12 10:49:59 -0500

    finshed

--- helloStudents.js
+++ helloStudents.js
@@ -1,9 +1,17 @@
 'use strict'
 
-// 1. Read the JSON file into a variable called students
+// This will read the JSON file and require it.
+let students = require('./students.json');
 
-// 2. Iterate over the students array and print Hello with their full names to the console
-// e.g. Hello Walter Baker
+// This will read over the student array and add Hello at the beginning.
+students.forEach(function (obj) {
+  console.log('Hello ' + obj.firstName + ' ' + obj.lastName);
+});
 
-// 3. Print out the number of last names starting with the letter D
-// e.g. Count of last names starting with D is 1
+// This will filter out the students with the last name starting with D.
+let studentFilter = students.filter((elem) => {
+  return elem.lastName.charAt(0) == 'D';
+});
+
+// Now it will spit out a message of how many hits it has received.
+console.log('Count of last names starting with D is ' + studentFilter.length);
